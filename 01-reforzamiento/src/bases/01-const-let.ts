9eb31f09c65a66c1ee904e3aa8b5e99b6b0ebb10
Fernando Zeballos <fernandodavidzeballos@gmail.com>
2025-12-18 01:05:32 +0000

    react-reforzamiento-variables-y-constantes

---
+++ 01-reforzamiento/src/bases/01-const-let.ts
@@ -0,0 +1,8 @@
+const firstName: string = 'Fernando';
+let lastName = 'Zeballos';
+
+let diceNumber = 5;
+diceNumber = 3
+const containsLetterZ = lastName.includes('Z')
+
+console.log({containsLetterZ, diceNumber, firstName, lastName})
