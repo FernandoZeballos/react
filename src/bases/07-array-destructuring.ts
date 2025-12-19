3cfc873db7a776d502f496dbb2d69cc10e6a626b
fernandodavidzeballos <fernandodavidzeballos@gmail.com>
2025-12-18 23:14:14 -0300

    finish_array_destructuring

---
+++ src/bases/07-array-destructuring.ts
@@ -0,0 +1,13 @@
+const characterNames: string[] = ['Goku','Vegeta','Trunks']
+
+const[, , Trunks] = characterNames;
+
+console.log({Trunks});
+
+const returnsArrayFn = () => {
+    return ['ABC', 123] as const;
+}
+
+const [letters, numbers] = returnsArrayFn();
+
+console.log({ letters, numbers});
