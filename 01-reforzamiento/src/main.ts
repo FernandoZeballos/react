c09d38a8e7ed6b9ecff096ede45b2eabcc2f4949
fernandodavidzeballos <fernandodavidzeballos@gmail.com>
2025-12-21 17:41:00 -0300

    02-estructura de directorio

--- 01-reforzamiento/src/main.ts
+++ 01-reforzamiento/src/main.ts
@@ -1,5 +1,17 @@
 import './style.css'
-import './bases/01-const-let';
+//import './bases/01-const-let';
+//import './bases/02-template-string';
+// import './bases/03-object-literal';
+//import './bases/04-arrays';
+// import './bases/05-functions';
+//import './bases/06-obj-destructuring';
+//import './bases/07-array-destructuring';
+//import './bases/08-imp-exp';
+//import './bases/09-promises';
+//import './bases/10-fetch-api';
+import './bases/11-async-await';
+
+//import { getHeroesByOwner } from './bases/08-imp-exp';
 
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
@@ -7,3 +19,5 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <h1>hola mundo</h1>
   </div>
 `
+
+//console.log(getHeroesByOwner('DC'))
---
+++ 02-first-steps/src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hola Mundo !!</h1>
+      <p>Hola Fernando</p>
+    </>
+  )
+}
+
+export default App
