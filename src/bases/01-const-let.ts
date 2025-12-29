fcc87b4aa5c39072d53a6d63c1db83b8d8aa19e3
fernandodavidzeballos <fernandodavidzeballos@gmail.com>
2025-12-29 16:30:12 -0300

    Limpieza: eliminando archivos raiz innecesarios para dejar solo carpetas de ejercicios

--- src/bases/01-const-let.ts
+++
@@ -1,8 +0,0 @@
-const firstName: string = 'Fernando';
-let lastName = 'Zeballos';
-
-let diceNumber = 5;
-diceNumber = 3
-const containsLetterZ = lastName.includes('Z')
-
-console.log({containsLetterZ, diceNumber, firstName, lastName})
